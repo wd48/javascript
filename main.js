ec6efed71466ac5edb700663cf77b7250ddec3c8
kastrio_it <wofyd465@gmail.com>
2021-11-17 23:48:20 +0900

    드림코딩 Javascript #2

---
+++ main.js
@@ -0,0 +1,12 @@
+// Whole-script strict mode syntax
+// JavaScript is very flexible
+// flexible === dangerous
+'use strict';
+// added ECMAScript 5
+
+console.log('Hello world');
+
+// API : 브라우저가 인식할 수 있는 함수의 모임
+
+let a1;
+a1 = 56;
